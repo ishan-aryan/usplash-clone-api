ba38206421fce9375cc09b7d62f626ba03a0db11
ishan-aryan-8882 <toxic.valorant.yt@gmail.com>
2024-03-10 15:41:41 +0530

    file save

--- script.js
+++ script.js
@@ -32,30 +32,52 @@ async function searchImg() {
       } else {
         insta_handle = "n/a";
       }
-      const imgContainer = $(`
-      <div class="img-cont">
-  <img src="${result.urls.small}" alt="${result.alt_description}" />
-  <a href="${result.links.download}" target="_blank" class="downld">
-    <i class="fa-solid fa-arrow-down"></i>
-  </a>
-  <div class="creator">
-    <div class="creator-img">
-      <img src="${result.user.profile_image.small}" alt="" />
-    </div>
-    <div class="creator-name">
-      <span>${result.user.first_name}</span>
-      <p>${insta_handle}</p>
-    </div>
-  </div>
-  <div class="heart">
-  <i class="fa-solid fa-heart"></i> 
-  </div>
-</div>
-                  `);
 
+      const imgContainer = $(`
+        <div class="img-cont">
+          <img src="${result.urls.small}" alt="${result.alt_description}" />
+          <a href="${result.links.download}" target="_blank" class="downld">
+            <i class="fa-solid fa-arrow-down"></i>
+          </a>
+          <div class="creator">
+            <div class="creator-img">
+              <img src="${result.user.profile_image.small}" alt="" />
+            </div>
+            <div class="creator-name">
+              <span>${result.user.first_name}</span>
+              <p>${insta_handle}</p>
+            </div>
+          </div>
+          <div class="heart">
+            <i class="fa-solid fa-heart"></i> 
+          </div>
+        </div>
+      `);
+
+      const smallBelowSection = $(
+        `<div class="below-img-cont">
+          <button class="heart-second">
+            <i class="fa-solid fa-heart"></i> 
+          </button>
+          <button class="download-second">
+            <p>Download</p>
+            <i class="fa-solid fa-chevron-down"></i>
+          </button>
+        </div>`
+      );
+
+      const smallAboveSection = $(`
+      <div class="creator-second">
+  <img src="${result.user.profile_image.small}" alt="" class="creator-img-second">
+  <p class="creator-name-second">${result.user.first_name}</p>
+</div>
+      `);
+
+      searchResults.append(smallAboveSection);
       searchResults.append(imgContainer);
-      console.log("hello");
+      searchResults.append(smallBelowSection);
     });
+
     console.log(url);
   } catch (error) {
     console.log("error is listed here");
@@ -97,33 +119,33 @@ var leftSideElement = $(".left-side-top");
 var computedHeight = leftSideElement.css("height");
 console.log("Computed height of left-side-top:", computedHeight);
 
-var rightSideBox = $('.right-side-box');
-rightBox.css('height',computedHeight);
+var rightSideBox = $(".right-side-box");
+rightBox.css("height", computedHeight);
 
-  function isTopSectionVisible() {
-    var topSection = $("#top-section");
-    var topSectionBounds = topSection[0].getBoundingClientRect();
-    return (
-      topSectionBounds.top >= 0 &&
-      topSectionBounds.left >= 0 &&
-      topSectionBounds.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      topSectionBounds.right <=
-        (window.innerWidth || document.documentElement.clientWidth)
-    );
+function isTopSectionVisible() {
+  var topSection = $("#top-section");
+  var topSectionBounds = topSection[0].getBoundingClientRect();
+  return (
+    topSectionBounds.top >= 0 &&
+    topSectionBounds.left >= 0 &&
+    topSectionBounds.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    topSectionBounds.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function toggleTopSectionReach() {
+  var topSectionReach = $("#top-reach-button");
+  if (isTopSectionVisible()) {
+    topSectionReach.css("scale", "0");
+  } else {
+    topSectionReach.css("scale", "1");
   }
+}
 
-  function toggleTopSectionReach() {
-    var topSectionReach = $("#top-reach-button");
-    if (isTopSectionVisible()) {
-      topSectionReach.css('scale','0');
-    } else {
-      topSectionReach.css('scale','1');
-    }
-  }
+toggleTopSectionReach();
 
+$(window).scroll(function () {
   toggleTopSectionReach();
-
-  $(window).scroll(function () {
-    toggleTopSectionReach();
-  });
+});
