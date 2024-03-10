8685d08204fa7f3054922ec047144bfda012afb4
ishan-aryan-8882 <toxic.valorant.yt@gmail.com>
2024-03-10 17:35:12 +0530

    file save

--- script.js
+++ script.js
@@ -67,6 +67,19 @@ async function searchImg() {
           </button>
         </div>`
       );
+      $(".heart-second").on("click",function () {
+        // Toggle class and styles
+        $(this).toggleClass("clik");
+        if ($(this).hasClass("clik")) {
+          $(this).find("i").css("color", "white");
+          $(this).css("background", "red");
+          $(this).css("border", "1px solid transparent");
+        } else {
+          $(this).find("i").css("color", "gray");
+          $(this).css("background", "white");
+          $(this).css("border", "1px solid rgba(128, 128, 128, 0.352)");
+        }
+      });
 
       const smallAboveSection = $(`
       <div class="creator-second">
